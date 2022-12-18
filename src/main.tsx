ee39813bab6f7c27e78e0ac8c6381b6e12fbac71
Zachary Lim <zacharylim2004@gmail.com>
2022-12-19 01:20:18 +0800

    Added Mantine UI providers

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
 import { createRoot } from "react-dom/client";
+import { MantineProvider } from "@mantine/core";
+import { NotificationsProvider } from "@mantine/notifications";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
-    <main>Hello World</main>
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+        <NotificationsProvider>
+            <main>Hello World</main>
+        </NotificationsProvider>
+    </MantineProvider>
 );

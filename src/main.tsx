f8cd857fda644bec0858ba36df3b31bdcc4710b7
Zachary Lim <zacharylim2004@gmail.com>
2022-12-19 16:53:01 +0800

    Added user provider to main file

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import { MantineProvider } from "@mantine/core";
 import { NotificationsProvider } from "@mantine/notifications";
 import { Router } from "./router";
+import { UserProvider } from "./components";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
     <MantineProvider
@@ -9,7 +10,9 @@ createRoot(document.getElementById("root") as HTMLElement).render(
         withNormalizeCSS
         theme={{ fontFamily: "Inter, sans-serif" }}>
         <NotificationsProvider>
-            <Router />
+            <UserProvider>
+                <Router />
+            </UserProvider>
         </NotificationsProvider>
     </MantineProvider>
 );

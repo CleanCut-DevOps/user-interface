ea2aaa7511aef3a058b51b63d4a5a5afd53d14b2
Zachary Lim <zacharylim2004@gmail.com>
2022-12-19 16:52:22 +0800

    Created React context API provider

---
+++ src/components/UserProvider.tsx
@@ -0,0 +1,72 @@
+import {
+    createContext,
+    Dispatch,
+    FC,
+    PropsWithChildren,
+    SetStateAction,
+    useEffect,
+    useState
+} from "react";
+import { User } from "../models";
+import { useCookies } from "react-cookie";
+import axios from "axios";
+
+type SchrodingersUser = User | null;
+
+type UserContextData = {
+    isLoading: boolean;
+    user: SchrodingersUser;
+    setUser: Dispatch<SetStateAction<SchrodingersUser>>;
+};
+
+export const UserContext = createContext<UserContextData>({
+    isLoading: true,
+    user: null,
+    setUser: () => {}
+});
+
+export const UserProvider: FC<PropsWithChildren> = ({ children }) => {
+    const [user, setUser] = useState<SchrodingersUser>(null);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+    const [cookies, setCookie] = useCookies(["AccessToken"]);
+
+    console.log(user);
+
+    useEffect(() => {
+        if (cookies.AccessToken) {
+            axios
+                .get("http://localhost:8001/api/account", {
+                    headers: { Authorization: `Bearer ${cookies.AccessToken}` }
+                })
+                .then(({ data }) => {
+                    console.log(data);
+                    setUser({
+                        id: data.id as string,
+                        email: data.email as string,
+                        username: data.username as string,
+                        contact: data.contact as string,
+                        avatar: data.avatar as string,
+                        type: data.type as "user" | "cleaner" | "admin",
+                        created_at: new Date(data.created_at),
+                        updated_at: new Date(data.updated_at)
+                    });
+
+                    setIsLoading(false);
+                })
+                .catch(err => {
+                    const { status } = err.response;
+                    const { type, message } = err.response.data;
+
+                    console.log({ status, type, message });
+
+                    setIsLoading(false);
+                });
+        } else setIsLoading(false);
+    }, [cookies, setIsLoading]);
+
+    return (
+        <UserContext.Provider value={{ isLoading, user, setUser }}>
+            {children}
+        </UserContext.Provider>
+    );
+};

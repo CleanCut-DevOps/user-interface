abbcfb00a7c80f7c1ce0c4d58950b8ac66d3fa33
Zachary <zacharylim2004@gmail.com>
2023-01-04 09:38:26 +0800

    🎨 Update provider for editting property

--- src/routes/Property/[...id]/edit/components/Provider.tsx
+++ src/routes/Property/[...id]/edit/components/Provider.tsx
@@ -5,45 +5,144 @@ import {
     Dispatch,
     FC,
     PropsWithChildren,
-    SetStateAction,
     useEffect,
-    useState
+    useReducer
 } from "react";
 import { useCookies } from "react-cookie";
+import { useLocation } from "wouter";
 import { Loading } from "../../../../../components";
-import { Property } from "../../../../../models";
-
-type SchrodingersProperty = Property | null;
-
-type PropertyResponse = {
-    type: string;
-    message: string;
-    property: Property;
-};
-
-type EditPropertyContextData = {
-    step: number;
-    setStep: Dispatch<SetStateAction<number>>;
-    property: SchrodingersProperty;
-    updateProperty: Dispatch<SetStateAction<SchrodingersProperty>>;
-};
-
-export const EditPropertyContext = createContext<EditPropertyContextData>({
-    step: 0,
-    setStep: () => {},
-    property: null,
-    updateProperty: () => {}
-});
+import { convertResponseToProperty, Property } from "../../../../../models";
 
 interface ComponentProps extends PropsWithChildren {
     id: string;
 }
 
+type SchrodingersProperty = Property | null;
+
+type ReducerAction =
+    | { type: "next"; payload?: null }
+    | { type: "previous"; payload?: null }
+    | { type: "setStep"; payload: number }
+    | { type: "load"; payload: Property }
+    | { type: "brief"; payload: { label: string; icon: string } }
+    | {
+          type: "details";
+          payload: {
+              icon: string;
+              label: string;
+              images: string[];
+              description: string | null;
+          };
+      }
+    | {
+          type: "address";
+          payload: {
+              line_1: string;
+              line_2: string | null;
+              city: string;
+              state: string | null;
+              postal_code: string;
+          };
+      }
+    | { type: "type"; payload: { id: string } }
+    | {
+          type: "rooms";
+          payload: {
+              id: string;
+              quantity: number;
+              updated_at: Date;
+          };
+      };
+
+type ReducerState = {
+    step: number;
+    loading: boolean;
+    property: SchrodingersProperty;
+};
+
+const initState: ReducerState = {
+    step: 0,
+    loading: true,
+    property: null
+};
+
+const reducer = (state: ReducerState, action: ReducerAction) => {
+    const { type } = action;
+
+    switch (type) {
+        case "next":
+            return { ...state, step: state.step + 1 };
+        case "previous":
+            return { ...state, step: state.step - 1 };
+        case "setStep":
+            const newStep = action.payload;
+            return { ...state, step: newStep };
+        case "load":
+            const loadPayload = action.payload;
+            return { step: 0, loading: false, property: loadPayload };
+        case "brief":
+            const briefPayload = action.payload;
+            return {
+                ...state,
+                property: state.property
+                    ? { ...state.property, ...briefPayload }
+                    : null
+            };
+        case "details":
+            const detailsPayload = action.payload;
+            return {
+                ...state,
+                property: state.property
+                    ? { ...state.property, ...detailsPayload }
+                    : null
+            };
+        case "address":
+            const addressPayload = action.payload;
+            return {
+                ...state,
+                property: state.property
+                    ? { ...state.property, ...addressPayload }
+                    : null
+            };
+        case "type":
+            const typePayload = action.payload;
+            return {
+                ...state,
+                property: state.property
+                    ? { ...state.property, ...typePayload }
+                    : null
+            };
+        case "rooms":
+            const roomsPayload = action.payload;
+            return {
+                ...state,
+                property: state.property
+                    ? { ...state.property, ...roomsPayload }
+                    : null
+            };
+        default:
+            return state;
+    }
+};
+
+type EditPropertyContextData = {
+    step: number;
+    property: SchrodingersProperty;
+    dispatch: Dispatch<ReducerAction>;
+};
+
+export const EditPropertyContext = createContext<EditPropertyContextData>({
+    step: 0,
+    property: null,
+    dispatch: () => {}
+});
+
 export const EditPropertyProvider: FC<ComponentProps> = ({ id, children }) => {
-    const [step, setStep] = useState<number>(0);
-    const [property, setProperty] = useState<SchrodingersProperty>(null);
-    const [isLoading, setLoading] = useState<boolean>(true);
     const [cookies] = useCookies(["AccessToken"]);
+    const [, setLocation] = useLocation();
+    const [state, dispatch] = useReducer(reducer, initState);
+
+    console.log(state);
 
     useEffect(() => {
         if (cookies.AccessToken) {
@@ -56,12 +155,10 @@ export const EditPropertyProvider: FC<ComponentProps> = ({ id, children }) => {
                         }
                     }
                 )
-                .then(({ data }: { data: PropertyResponse }) => {
-                    const { property } = data;
+                .then(({ data }) => {
+                    const property = convertResponseToProperty(data.property);
 
-                    setProperty(property);
-
-                    setLoading(false);
+                    dispatch({ type: "load", payload: property });
                 })
                 .catch(({ response: { data } }) => {
                     showNotification({
@@ -70,21 +167,24 @@ export const EditPropertyProvider: FC<ComponentProps> = ({ id, children }) => {
                         color: "red"
                     });
 
-                    setLoading(false);
+                    setLocation("/");
                 });
         } else {
-            setProperty(null);
-            setLoading(false);
-        }
-    }, [cookies, setLoading, setProperty, showNotification]);
+            showNotification({
+                title: "No Access Token",
+                message: "Please refresh the site and login to continue 🙂",
+                color: "red"
+            });
 
-    const updateProperty = () => {};
+            setLocation("/");
+        }
+    }, []);
 
     return (
         <EditPropertyContext.Provider
-            value={{ step, setStep, property, updateProperty }}
+            value={{ step: state.step, property: state.property, dispatch }}
         >
-            {isLoading ? <Loading /> : children}
+            {state.loading ? <Loading /> : children}
         </EditPropertyContext.Provider>
     );
 };

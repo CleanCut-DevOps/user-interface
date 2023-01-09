a36751e33bbd364a78c052d20560e73b83295119
Zachary <zacharylim2004@gmail.com>
2023-01-09 19:40:41 +0800

    ✨ Added new saving attribute to editting context

--- src/routes/Property/[...id]/edit/components/Provider.tsx
+++ src/routes/Property/[...id]/edit/components/Provider.tsx
@@ -1,13 +1,7 @@
+import { useDebouncedValue } from "@mantine/hooks";
 import { showNotification } from "@mantine/notifications";
 import axios from "axios";
-import {
-    createContext,
-    Dispatch,
-    FC,
-    PropsWithChildren,
-    useEffect,
-    useReducer
-} from "react";
+import { createContext, Dispatch, FC, PropsWithChildren, useEffect, useReducer } from "react";
 import { useCookies } from "react-cookie";
 import { useLocation } from "wouter";
 import { Loading } from "../../../../../components";
@@ -23,6 +17,7 @@ type ReducerAction =
     | { type: "next"; payload?: null }
     | { type: "previous"; payload?: null }
     | { type: "setStep"; payload: number }
+    | { type: "setSaving"; payload: { newState: boolean; cancel?: () => void } }
     | { type: "load"; payload: Property }
     | { type: "brief"; payload: { label: string; icon: string } }
     | {
@@ -56,17 +51,19 @@ type ReducerAction =
 
 type ReducerState = {
     step: number;
+    saving: boolean;
     loading: boolean;
     property: SchrodingersProperty;
 };
 
 const initState: ReducerState = {
     step: 0,
+    saving: false,
     loading: true,
     property: null
 };
 
-const reducer = (state: ReducerState, action: ReducerAction) => {
+const reducer = (state: ReducerState, action: ReducerAction): ReducerState => {
     const { type } = action;
 
     switch (type) {
@@ -77,48 +74,49 @@ const reducer = (state: ReducerState, action: ReducerAction) => {
         case "setStep":
             const newStep = action.payload;
             return { ...state, step: newStep };
+        case "setSaving":
+            const newSaveState = action.payload.newState;
+
+            action.payload.cancel?.();
+
+            return { ...state, saving: newSaveState };
         case "load":
             const loadPayload = action.payload;
-            return { step: 0, loading: false, property: loadPayload };
+            return {
+                ...state,
+                loading: false,
+                saving: false,
+                property: loadPayload
+            };
         case "brief":
             const briefPayload = action.payload;
             return {
                 ...state,
-                property: state.property
-                    ? { ...state.property, ...briefPayload }
-                    : null
+                property: state.property ? { ...state.property, ...briefPayload } : null
             };
         case "details":
             const detailsPayload = action.payload;
             return {
                 ...state,
-                property: state.property
-                    ? { ...state.property, ...detailsPayload }
-                    : null
+                property: state.property ? { ...state.property, ...detailsPayload } : null
             };
         case "address":
             const addressPayload = action.payload;
             return {
                 ...state,
-                property: state.property
-                    ? { ...state.property, ...addressPayload }
-                    : null
+                property: state.property ? { ...state.property, ...addressPayload } : null
             };
         case "type":
             const typePayload = action.payload;
             return {
                 ...state,
-                property: state.property
-                    ? { ...state.property, ...typePayload }
-                    : null
+                property: state.property ? { ...state.property, ...typePayload } : null
             };
         case "rooms":
             const roomsPayload = action.payload;
             return {
                 ...state,
-                property: state.property
-                    ? { ...state.property, ...roomsPayload }
-                    : null
+                property: state.property ? { ...state.property, ...roomsPayload } : null
             };
         default:
             return state;
@@ -127,34 +125,36 @@ const reducer = (state: ReducerState, action: ReducerAction) => {
 
 type EditPropertyContextData = {
     step: number;
+    saving: boolean;
+    forceSave: () => void;
+    localMismatch: boolean;
     property: SchrodingersProperty;
     dispatch: Dispatch<ReducerAction>;
 };
 
 export const EditPropertyContext = createContext<EditPropertyContextData>({
     step: 0,
+    saving: false,
     property: null,
-    dispatch: () => {}
+    localMismatch: false,
+    dispatch: () => {},
+    forceSave: () => {}
 });
 
 export const EditPropertyProvider: FC<ComponentProps> = ({ id, children }) => {
     const [cookies] = useCookies(["AccessToken"]);
     const [, setLocation] = useLocation();
     const [state, dispatch] = useReducer(reducer, initState);
-
-    console.log(state);
+    const [propertyDebounced, cancel] = useDebouncedValue(state.property, 30000);
 
     useEffect(() => {
         if (cookies.AccessToken) {
             axios
-                .get(
-                    `${import.meta.env.VITE_PROPERTY_API}/api/property/${id}`,
-                    {
-                        headers: {
-                            Authorization: `Bearer ${cookies.AccessToken}`
-                        }
+                .get(`${import.meta.env.VITE_PROPERTY_API}/property/${id}`, {
+                    headers: {
+                        Authorization: `Bearer ${cookies.AccessToken}`
                     }
-                )
+                })
                 .then(({ data }) => {
                     const property = convertResponseToProperty(data.property);
 
@@ -180,9 +180,61 @@ export const EditPropertyProvider: FC<ComponentProps> = ({ id, children }) => {
         }
     }, []);
 
+    useEffect(() => {
+        if (!state.loading) updateProperty();
+    }, [propertyDebounced]);
+
+    const updateProperty = () => {
+        if (cookies.AccessToken) {
+            if (!state.saving) {
+                if (propertyDebounced !== state.property) {
+                    dispatch({ type: "setSaving", payload: { newState: true, cancel } });
+                } else {
+                    dispatch({ type: "setSaving", payload: { newState: false } });
+                }
+
+                axios
+                    .put(`${import.meta.env.VITE_PROPERTY_API}/property/${id}`, state.property, {
+                        headers: {
+                            Authorization: `Bearer ${cookies.AccessToken}`
+                        }
+                    })
+                    .then(({ data }) => {
+                        const property = convertResponseToProperty(data.property);
+
+                        dispatch({ type: "load", payload: property });
+                    })
+                    .catch(({ response: { data } }) => {
+                        dispatch({ type: "setSaving", payload: { newState: false } });
+
+                        showNotification({
+                            title: data.type,
+                            message: `${data.message}. Please try again after fixing the issue.`,
+                            color: "red"
+                        });
+                    });
+            }
+        } else {
+            showNotification({
+                title: "No Access Token",
+                message: "Please refresh the site and login to continue 🙂",
+                color: "red"
+            });
+
+            setLocation("/");
+        }
+    };
+
     return (
         <EditPropertyContext.Provider
-            value={{ step: state.step, property: state.property, dispatch }}
+            value={{
+                dispatch,
+                step: state.step,
+                saving: state.saving,
+                property: state.property,
+                forceSave: updateProperty,
+                localMismatch: propertyDebounced !== state.property
+            }}
         >
             {state.loading ? <Loading /> : children}
         </EditPropertyContext.Provider>

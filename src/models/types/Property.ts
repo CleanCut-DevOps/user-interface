cef7f25267f23b55f96d1a2dc366a14ffd494a93
Zachary <zacharylim2004@gmail.com>
2023-01-12 01:08:52 +0800

    Updated property models

--- src/models/types/Property.ts
+++ src/models/types/Property.ts
@@ -1,12 +1,9 @@
-import { RoomType } from "./Room";
-
 export type PropertyType = {
     id: string;
     label: string;
     available: boolean;
     description: string;
     detailed_description: string;
-    rooms: RoomType[];
     created_at: Date;
     updated_at: Date;
 };
--- src/models/types/Service.ts
+++ src/models/types/Service.ts
@@ -2,8 +2,11 @@ export type ServiceType = {
     id: string;
     label: string;
     description: string;
+    detailed_description: string;
     category: string;
     price: number;
     quantifiable: boolean;
     available: boolean;
+    created_at: Date;
+    updated_at: Date;
 };

f10b9036ff5818d271b33c26bf7f3da9515af645
Zachary <zacharylim2004@gmail.com>
2023-01-21 15:29:42 +0800

    🎉 Initialize new files

---
+++ src/main.tsx
@@ -0,0 +1,31 @@
+import { ColorScheme, ColorSchemeProvider, MantineProvider } from "@mantine/core";
+import { ModalsProvider } from "@mantine/modals";
+import { NotificationsProvider } from "@mantine/notifications";
+import { FC, useState } from "react";
+import { useCookies } from "react-cookie";
+import { createRoot } from "react-dom/client";
+
+const Main: FC = () => {
+    const [cookie, setCookie] = useCookies(["mantine-color-scheme"]);
+    const [colorScheme, setColorScheme] = useState<ColorScheme>(cookie["mantine-color-scheme"]);
+
+    const toggleColorScheme = (value?: ColorScheme) => {
+        let newScheme = value ?? (colorScheme === "dark" ? "light" : "dark");
+        setColorScheme(newScheme);
+        setCookie("mantine-color-scheme", newScheme, { maxAge: 60 * 60 * 24 * 30 });
+    };
+
+    return (
+        <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+            <MantineProvider withGlobalStyles withNormalizeCSS theme={{ fontFamily: "Inter, sans-serif", colorScheme }}>
+                <NotificationsProvider>
+                    <ModalsProvider>
+                        <>App</>
+                    </ModalsProvider>
+                </NotificationsProvider>
+            </MantineProvider>
+        </ColorSchemeProvider>
+    );
+};
+
+createRoot(document.getElementById("root") as HTMLElement).render(<Main />);

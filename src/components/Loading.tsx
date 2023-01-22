baa5039a9d19da3b544f1f9836d3b1d577cc3ca4
Zachary <zacharylim2004@gmail.com>
2023-01-22 11:28:17 +0800

    ✨ Update loading component

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,5 +1,35 @@
+import { Center, createStyles, Loader, Stack, Text, Title } from "@mantine/core";
 import { FC } from "react";
 
+const useStyles = createStyles(theme => ({
+    wrapper: {
+        width: "100vw",
+        height: "100vh"
+    },
+    header: {
+        padding: theme.spacing.sm,
+        border: `1px solid ${theme.colorScheme === "dark" ? theme.colors.dark[6] : theme.colors.gray[2]}`
+    },
+    content: {
+        flex: 1,
+        height: "100%",
+        gap: theme.spacing.md
+    }
+}));
+
 export const Loading: FC = () => {
-    return <>component</>;
+    const { classes } = useStyles();
+    return (
+        <Stack className={classes.wrapper}>
+            <div className={classes.header}>
+                <Text size="lg" weight={600}>
+                    CleanCut
+                </Text>
+            </div>
+            <Center className={classes.content}>
+                <Loader variant="bars" color="indigo" size={32} />
+                <Text>Getting things ready for you</Text>
+            </Center>
+        </Stack>
+    );
 };

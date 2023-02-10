0002b02bbadf0348071e77cc8734ca09d012138c
Zachary <zacharylim2004@gmail.com>
2023-02-10 15:50:02 +0800

    ✨ Created SWR hooks for bookings

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -33,6 +33,33 @@ export const useProperty = (id: string, accessToken?: string) => {
     };
 };
 
+export const useBookings = (accessToken?: string) => {
+    const { data, error, isLoading } = useSWR(
+        [`${import.meta.env.VITE_BOOKING_API}/bookings`, accessToken],
+        ([url, token]) => fetchWithToken(url, token),
+        { refreshInterval: 2000 }
+    );
+
+    return {
+        data: data,
+        isLoading,
+        isError: error
+    };
+};
+
+export const useBooking = (id: string, accessToken?: string) => {
+    const { data, error, isLoading } = useSWR(
+        [`${import.meta.env.VITE_BOOKING_API}/bookings/${id}`, accessToken],
+        ([url, token]) => fetchWithToken(url, token)
+    );
+
+    return {
+        data: data,
+        isLoading,
+        isError: error
+    };
+};
+
 export const useUserData = (accessToken?: string) => {
     const { data, error, isLoading } = useSWR(
         [`${import.meta.env.VITE_ACCOUNT_API}/user`, accessToken],

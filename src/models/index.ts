ca193fb997fec79cf9dd6036b8c0d97f3f873f11
Zachary <zacharylim2004@gmail.com>
2023-02-10 15:49:48 +0800

    ⚡️ Updated models to use utility types

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,7 +1,9 @@
+export { convertResponseToBooking } from "./Booking";
+export type { Booking } from "./Booking";
 export { convertResponseToProperty } from "./Property";
 export type { Property, Room } from "./Property";
 export type { PropertyType } from "./types/Property";
 export type { RoomType } from "./types/Room";
-export type { ServiceType } from "./types/Service";
+export type { ServiceType, Product } from "./types/Service";
 export { convertResponseToUser } from "./User";
 export type { User } from "./User";
--- src/models/types/Service.ts
+++ src/models/types/Service.ts
@@ -2,10 +2,15 @@ export type ServiceType = {
     id: string;
     label: string;
     price: number;
-    category: string;
-    description: string;
     quantifiable: boolean;
     available: boolean;
     created_at: Date;
     updated_at: Date;
+    category: string;
+    products: Product[];
+};
+
+export type Product = {
+    label: string;
+    quantity: number;
 };
